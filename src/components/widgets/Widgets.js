548a94a05a3e69fc5bdde37322b8bcd728c352d3
Isreal6ix <isrealsix@gmail.com>
2021-08-16 16:20:57 +0700

    completed the widget component

--- src/components/widgets/Widgets.js
+++ src/components/widgets/Widgets.js
@@ -1,8 +1,30 @@
 import './Widgets.scss';
+import {
+	TwitterTimelineEmbed,
+	TwitterShareButton,
+	TwitterTweetEmbed,
+} from 'react-twitter-embed';
+import { Search } from '@material-ui/icons';
+
 const Widgets = () => {
 	return (
 		<div className="widgets">
-			<h2>Widgets</h2>
+			<div className="widgets__input">
+				<Search className="widgets__searchIcon" />
+				<input placeholder="Search Twitter" type="text" />
+			</div>
+
+			<div className="widgets__widgetContainer">
+				<h2>What's Happening?</h2>
+
+				<TwitterTweetEmbed tweetId={'1416034083382169600'} />
+
+				<TwitterTimelineEmbed
+					sourceType="profile"
+					screenName="isrealsix"
+					options={{ height: 400 }}
+				/>
+			</div>
 		</div>
 	);
 };
