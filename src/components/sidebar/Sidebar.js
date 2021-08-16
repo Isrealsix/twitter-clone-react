376aa692a9e1aae9bc4e8dff96b55d081564ad1b
Isreal6ix <isrealsix@gmail.com>
2021-08-16 13:26:37 +0700

    completed the sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import {
+	Twitter,
+	Home,
+	Search,
+	NotificationsNone,
+	MailOutline,
+	BookmarkBorder,
+	ListAlt,
+	PermIdentity,
+	MoreHoriz,
+} from '@material-ui/icons';
 import './Sidebar.css';
+import SidebarOption from './sidebarOption/SidebarOption';
+import { Button } from '@material-ui/core';
 
 const Sidebar = () => {
-	return <div className="sidebar"></div>;
+	return (
+		<div className="sidebar">
+			<Twitter />
+
+			<SidebarOption active Icon={Home} text="Home" />
+			<SidebarOption Icon={Search} text="Explore" />
+			<SidebarOption Icon={NotificationsNone} text="Notifications" />
+			<SidebarOption Icon={MailOutline} text="Messages" />
+			<SidebarOption Icon={BookmarkBorder} text="Bookmarks" />
+			<SidebarOption Icon={ListAlt} text="Lists" />
+			<SidebarOption Icon={PermIdentity} text="Profile" />
+			<SidebarOption Icon={MoreHoriz} text="More" />
+
+			<Button varient="outlined" fullWidth className="sidebar__tweet">
+				Tweet
+			</Button>
+		</div>
+	);
 };
 
 export default Sidebar;
